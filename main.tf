b6e093c377fb9b3d7c73186da6f6ae49db92e348
Ladi4U <128870718+Ladi4U@users.noreply.github.com>
2023-09-29 19:47:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplestoracc"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.mcit.name
+  location                 = azurerm_resource_group.mcit.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
