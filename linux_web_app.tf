02724c428cc91856f4337cd8ed1a2b525cdecbc4
Ladi4U <128870718+Ladi4U@users.noreply.github.com>
2023-10-02 20:02:37 -0400

    Update linux_web_app.tf

--- linux_web_app.tf
+++ linux_web_app.tf
@@ -14,7 +14,7 @@ resource "azurerm_service_plan" "mcit" {
 resource "azurerm_linux_web_app" "mcit" {
   name                = "mcit"
   resource_group_name = azurerm_resource_group.linuxwebapp.name
-  location            = azurerm_service_plan.linuxwebapp.location
+  location            = azurerm_resource_group.linuxwebapp.location
   service_plan_id     = azurerm_service_plan.mcit.id
 
   site_config {}
