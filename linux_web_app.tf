202a484fb91b4e02228321abfd14725825ec0d62
Ladi4U <128870718+Ladi4U@users.noreply.github.com>
2023-10-02 20:14:17 -0400

    Update linux_web_app.tf

--- linux_web_app.tf
+++ linux_web_app.tf
@@ -1,4 +1,8 @@
+locals{
+machine_name= ["machine11","machine22","machine33","machine44","machine55"]
+}
 resource "azurerm_resource_group" "linuxwebapp" {
+for_each= azurerm_resource_group.linuxwebapp 
   name     = "mcit-resources"
   location = "West Europe"
 }
@@ -19,3 +23,4 @@ resource "azurerm_linux_web_app" "mcit" {
 
   site_config {}
 }
+
